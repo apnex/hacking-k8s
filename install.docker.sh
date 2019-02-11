a5a465715f32b1b0817103ae84338872c47bd8f7
root <aobersnel@apnex.com.au>
2019-02-12 07:57:50 +1100

    Minor updates...

--- install.docker.sh
+++ install.docker.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+## check yum latest
 yum check-update
 
 ## Now run this command. It will add the official Docker repository, download the latest version of Docker, and install it:
-
 curl -fsSL https://get.docker.com/ | sh
 
 ## After installation has completed, start the Docker daemon:
-
 systemctl start docker
 
 ### Verify that it's running:
-
 systemctl status docker
 
 ### The output should be similar to the following, showing that the service is active and running:
@@ -22,6 +20,6 @@ systemctl status docker
 #   Active: active (running) since Sun 2016-05-01 06:53:52 CDT; 1 weeks 3 days ago
 #     Docs: https://docs.docker.com
 # Main PID: 749 (docker)
-#Lastly, make sure it starts at every server reboot:
 
+# Lastly, make sure it starts at every server reboot:
 systemctl enable docker
